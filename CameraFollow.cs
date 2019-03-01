1cecfaa7b5d102ee1fcd246a49ced8728f1f390f
Alexander Shirokih <alexandershirokih98@gmail.com>
2019-03-01 21:43:30 +0300

    Updated script

---
+++ CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Mutanium
+{
+    public class CameraFollow : MonoBehaviour
+    {
+
+        public Transform target;
+
+        public float smoothing = 5f;
+
+        private float distance;
+
+        private float xAngle;
+
+        public float yAngle = 25f;
+
+        public float rotationSpeed = 5f;
+
+        void Start()
+        {
+            distance = Vector3.Distance(transform.position, target.position);
+        }
+
+        void LateUpdate()
+        {
+            xAngle += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+
+            Quaternion rot = Quaternion.Lerp(transform.rotation, Quaternion.Euler(yAngle, xAngle, 0), Time.deltaTime * smoothing);
+            var newPos = target.position - rot * (Vector3.forward * distance);
+
+            transform.position = newPos;
+            transform.rotation = rot;
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody mRigidbody;
+
+    public float rotationSpeed = 5f;
+
+    private Vector3 moveDir;
+
+    public float speed = 10f;
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+        moveDir = Vector3.forward;
+    }
+
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        moveDir.Set(h, 0, v);
+        moveDir = moveDir.normalized * speed * Time.deltaTime;
+
+        mRigidbody.MovePosition(transform.position + moveDir);
+    }
+}
